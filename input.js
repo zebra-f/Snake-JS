3047eb614cad8f757e746ae1e367650ef3a28e54
Rafal Zebrowski <rafal.zebrowski@gmail.com>
2021-12-25 17:45:48 +0100

    keyboard input

--- input.js
+++ input.js
@@ -1,22 +1,36 @@
-let inputDirection = { x: 0, y: 0}
+let inputDirection = { x: 0, y: 0};
+let lastInputDirection = { x: 0, y: 0};
 
 window.addEventListener('keydown', e => {
     switch (e.key) {
+        case "w":
         case "ArrowUp":
-            inputDirection = { x: 0, y: -1 }
-            break
+            if (lastInputDirection.y !== 0) break;
+            inputDirection = { x: 0, y: -1 };
+            break;
+        case "s":
         case "ArrowDown":
-            inputDirection = { x: 0, y: 1 }
-            break
+            if (lastInputDirection.y !== 0) break;
+            inputDirection = { x: 0, y: 1 };
+            break;
+        case "d":
         case "ArrowRight":
-            inputDirection = { x: 1, y: 0 }
-            break
+            if (lastInputDirection.x !== 0) break;
+            inputDirection = { x: 1, y: 0 };
+            break;
+        case "a":
         case "ArrowLeft":
-            inputDirection = { x: -1, y: 0 }
-            break
+            if (lastInputDirection.x !== 0) break;
+            inputDirection = { x: -1, y: 0 };
+            break;
     }
 });
 
+// window.addEventListener('keydown', e => {
+//     console.log(e.key);
+// });
+
 export let getInputDirection = () => {
-    return inputDirection
+    lastInputDirection = inputDirection;
+    return inputDirection;
 }
